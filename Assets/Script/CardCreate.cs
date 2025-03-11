ea404c86af1ad6eecd89f46855329c36c1ee8f7f
MSI\daiku <123cr0007@tokyo.amg.ac.jp>
2025-03-11 16:12:58 +0900

    画像を読み取りを追加

--- Assets/Script/CardCreate.cs
+++ Assets/Script/CardCreate.cs
@@ -10,7 +10,7 @@ public class CardCreate : MonoBehaviour
 	// グリッド（フィールド）
 	public Transform grid;
 
-	private int cardNumber = 13;
+	private int cardNumber = 1;
 
 	// Start is called before the first frame update
 	void Start()
@@ -24,8 +24,35 @@ public class CardCreate : MonoBehaviour
 
 		for(int i = 0; i < cardNumber; i++)
 		{
+			// カードの番号
 			string cardNum = i.ToString("00");//数値を桁数指定で文字列に変換
-			Debug.Log(cardNum);
+
+			// カードのマーク
+			string spade = "spade";
+			string heart = "heart";
+			string diamond = "diamond";
+			string clover = "clover";
+
+			// カードの画像の名前
+			string cardImageSpade = cardNum + "_" + spade;
+			string cardImageHeart = cardNum + "_" + heart;
+			string cardImageDiamond = cardNum + "_" + diamond;
+			string cardImageClub = cardNum + "_" + clover;
+
+			Debug.Log(cardImageSpade);
+			Debug.Log("Image/" + spade + "/" + cardImageSpade);
+
+			// カードの画像をロード
+			Sprite cardImage = Resources.Load<Sprite>("Image/" + spade + "/" + cardImageSpade);
+			Debug.Log(cardImage);
+			if (cardImage != null) cardImageList.Add(cardImage);
+			cardImage = Resources.Load<Sprite>("Image/" + heart + "/" + cardImageHeart);
+			if (cardImage != null) cardImageList.Add(cardImage);
+			cardImage = Resources.Load<Sprite>("Image/" + diamond + "/" + cardImageDiamond);
+			if (cardImage != null) cardImageList.Add(cardImage);
+			cardImage = Resources.Load<Sprite>("Image/" + clover + "/" + cardImageClub);
+			if (cardImage != null) cardImageList.Add(cardImage);
 		}
+		Debug.Log(cardImageList.Count);
 	}
 }
