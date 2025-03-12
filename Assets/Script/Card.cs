bcaf755ded2a6a3894af95284fa4420d61321e05
MSI\daiku <123cr0007@tokyo.amg.ac.jp>
2025-03-12 16:06:55 +0900

    裏面の追加、表にして同じだったら消す、違ったら戻す処理の追加

--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -14,22 +14,39 @@ public class Card : MonoBehaviour
 	// 選択されているかどうか
 	public bool isSelected;
 
+	// カードの情報
+	private CardInfo cardInfo;
+
 	// カードの情報をセットする
 	public void SetCardInfo(CardInfo cardInfo)
 	{
+		this.cardInfo = cardInfo;
 		this.cardNumber = cardInfo.cardNumber;
-		this.cardImage.sprite = cardInfo.cardImage;
+		this.cardImage.sprite = Resources.Load<Sprite>("Image/Back");
 		this.isSelected = false;
 	}
 
 	// ボタンが押されたときの処理
 	public void OnClick()
 	{
+		// 選択されている数が二枚以上の場合は処理しない
+		if (CardManager.cardManagerInstance.frontCardList.Count >= 2)
+		{
+			return;
+		}
+
 		// 選択されているかどうか
 		if (!this.isSelected)
 		{
 			// 選択されている場合は非選択状態にする
 			this.isSelected = true;
+
+			// イメージを変更
+			this.cardImage.sprite = cardInfo.cardImage;
+
+			// managerに選択されたカードを渡す
+			CardManager.cardManagerInstance.frontCardList.Add(this);
+
 			Debug.Log("選択されました");
 		}
 	}
---
+++ Assets/Script/CardManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// カードのマネージャー
+//先に処理させたい
+[DefaultExecutionOrder(-1)]
+public class CardManager : MonoBehaviour
+{
+	// インスタンス
+	public static CardManager cardManagerInstance;
+
+	// 表のカードのリスト
+	public List<Card> frontCardList;
+
+	// 裏返すカウント
+	public int turnCount;
+	private const int TURN_MAX_CNT = 60;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		// インスタンスが存在しない場合
+		if (cardManagerInstance == null)
+		{
+			// このオブジェクトをインスタンスにする
+			cardManagerInstance = this;
+		}
+
+		// リストを初期化
+		frontCardList = new List<Card>();
+
+		// カウントを初期化
+		turnCount = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		// 表のカードが2枚以上の場合
+		if (frontCardList.Count >= 2)
+		{
+			if(turnCount > TURN_MAX_CNT)
+			{
+				// カードの番号が同じ場合
+				if (frontCardList[0].cardNumber == frontCardList[1].cardNumber)
+				{
+					// カードを消す
+					Destroy(frontCardList[0].gameObject);
+					Destroy(frontCardList[1].gameObject);
+				}
+				else
+				{
+					// カードを裏にする
+					frontCardList[0].cardImage.sprite = Resources.Load<Sprite>("Image/Back");
+					frontCardList[1].cardImage.sprite = Resources.Load<Sprite>("Image/Back");
+
+					// カードの選択を解除
+					frontCardList[0].isSelected = false;
+					frontCardList[1].isSelected = false;
+				}
+
+				// 初期化
+				frontCardList.Clear();
+				turnCount = 0;
+			}
+			else
+			{
+				turnCount++;
+			}
+
+		}
+	}
+}
